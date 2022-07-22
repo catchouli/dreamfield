9c9ffcbf22c5d6568250e47f2ff143fa04d9f116
Caitlin Wilks <cat@cat.bio>
2022-07-22 08:48:30 +0100

    simplify input handlning

--- src/main.rs
+++ src/main.rs
@@ -32,10 +32,6 @@ fn main() {
     let (win_width, win_height) = window.window.get_size();
     let mut renderer = GLRenderer::new(win_width, win_height);
 
-    // The input state
-    let mut input_events = VecDeque::<InputEvent>::new();
-
-    // The game state
     let mut game_state = RewindableGameState::<GameState>::new();
 
     // Fixed timestep - https://gafferongames.com/post/fix_your_timestep/
@@ -53,16 +49,11 @@ fn main() {
     while !window.window.should_close() {
         // Handle events
         for event in window.poll_events() {
-            handle_window_event(&mut window, &mut renderer, event, &mut input_events);
+            handle_window_event(&mut window, &mut renderer, event, &mut input_state);
         }
 
         // Handle mouse movement
-        (mouse_x, mouse_y) = handle_mouse_movement(&window, (mouse_x, mouse_y), &mut input_events);
-
-        // Update input state
-        for event in input_events.iter() {
-            handle_input_event(*event, &mut input_state)
-        }
+        (mouse_x, mouse_y) = handle_mouse_movement(&window, (mouse_x, mouse_y), &mut input_state);
 
         // Fixed timestep
         let new_time = window.glfw.get_time();
@@ -88,7 +79,7 @@ fn main() {
 
 /// Handle events
 fn handle_window_event(window: &mut Window, renderer: &mut GLRenderer, event: glfw::WindowEvent,
-                       input_events: &mut VecDeque<InputEvent>)
+                       input_state: &mut InputState)
 {
     match event {
         glfw::WindowEvent::FramebufferSize(width, height) => {
@@ -100,23 +91,23 @@ fn handle_window_event(window: &mut Window, renderer: &mut GLRenderer, event: gl
         glfw::WindowEvent::MouseButton(_, Action::Press, _) => {
             if !window.is_mouse_captured() {
                 window.set_mouse_captured(true);
-                input_events.push_back(InputEvent::CursorCaptured(true));
+                input_state.cursor_captured = true;
             }
         }
         glfw::WindowEvent::Key(Key::LeftAlt, _, Action::Press, _) | glfw::WindowEvent::Focus(false) => {
             if window.is_mouse_captured() {
                 window.set_mouse_captured(false);
-                input_events.push_back(InputEvent::CursorCaptured(false));
+                input_state.cursor_captured = false;
             }
         }
         glfw::WindowEvent::Key(key, _, Action::Press, _) => {
             if let Some(input) = map_game_inputs_key(key) {
-                input_events.push_back(InputEvent::GameInput(input, true));
+                input_state.inputs[input as usize] = true;
             }
         }
         glfw::WindowEvent::Key(key, _, Action::Release, _) => {
             if let Some(input) = map_game_inputs_key(key) {
-                input_events.push_back(InputEvent::GameInput(input, false));
+                input_state.inputs[input as usize] = false;
             }
         }
         _ => {}
@@ -138,29 +129,12 @@ fn map_game_inputs_key(key: Key) -> Option<InputName> {
 
 /// Handle mouse movement
 fn handle_mouse_movement(window: &Window, (old_mouse_x, old_mouse_y): (f64, f64),
-                         input_events: &mut VecDeque<InputEvent>) -> (f64, f64)
+                         input_state: &mut InputState) -> (f64, f64)
 {
     let (mouse_x, mouse_y) = window.window.get_cursor_pos();
     let (mouse_dx, mouse_dy) = (mouse_x - old_mouse_x, mouse_y - old_mouse_y);
 
-    if window.is_mouse_captured() && (mouse_dx != 0.0 || mouse_dy != 0.0) {
-        input_events.push_back(InputEvent::CursorMoved(mouse_dx, mouse_dy));
-    }
+    input_state.mouse_diff = (mouse_dx, mouse_dy);
 
     (mouse_x, mouse_y)
-}
-
-/// Handle an input event
-fn handle_input_event(event: InputEvent, input_state: &mut InputState) {
-    match event {
-        InputEvent::CursorMoved(dx, dy) => {
-            input_state.mouse_diff = (dx, dy);
-        }
-        InputEvent::CursorCaptured(captured) => {
-            input_state.cursor_captured = captured;
-        }
-        InputEvent::GameInput(input_name, is_down) => {
-            input_state.inputs[input_name as usize] = is_down;
-        }
-    }
 }
